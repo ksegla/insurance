ca7ac32ea5e4b31f34c56c9b65a891af59bb8bf1
ksegla <jeanluc.kpodjedo@gmail.com>
2016-03-08 23:28:00 -0500

    2

--- src/insurance/Person.java
+++ src/insurance/Person.java
@@ -1,5 +1,47 @@
 package insurance;
 
-public class Person {
+import java.util.Date;
 
+public class Person {
+	String firstName;
+	String lastName;
+	String address;
+	String zipcode;
+	Date birthDate;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+	
+	
+	
 }
